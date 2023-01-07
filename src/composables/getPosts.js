6f1135803c15392d168819faccc39eda61eff7ae
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-01-07 15:50:41 +0100

    getting data from firebase collection done

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue';
+import { projectFirestore } from '../firebase/firebase-config';
 
 const getPosts = () => {
   const posts = ref([]);
@@ -6,16 +7,21 @@ const getPosts = () => {
 
   const load = async () => {
     try {
-      await new Promise((resolve) => {
-        setTimeout(resolve, 2000);
-      });
+      // await new Promise((resolve) => {
+      //   setTimeout(resolve, 2000);
+      // });
 
-      let data = await fetch(' http://localhost:3000/posts');
-      console.log(data);
-      if (!data.ok) {
-        throw Error('No data available for now!');
-      }
-      posts.value = await data.json();
+      // let data = await fetch(' http://localhost:3000/posts');
+      // console.log(data);
+      // if (!data.ok) {
+      //   throw Error('No data available for now!');
+      // }
+      // posts.value = await data.json();
+      const res = await projectFirestore.collection('posts').get();
+
+      posts.value = res.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
     } catch (err) {
       error.value = err.message;
 
